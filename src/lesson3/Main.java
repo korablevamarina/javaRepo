1f3942ababc7d3e888418ca6818fe7c411d18ca9
JavaCore <korablevamarina2412@gmail.com>
2021-09-22 01:20:46 +0300

    main branch refactor

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -1,4 +1,18 @@
 package lesson3;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        String[] arr = {"a", "b", "c"};
+        method(arr,0, 1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+    private static void method(Object[] arr, int x, int y) {
+        Object buf = arr[x];
+        arr[x] = arr[y];
+        arr[y] = buf;
+    }
+
 }
