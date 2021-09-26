4179beb2498d4ea474416e9a3ead34603aae11f2
JavaCore <korablevamarina2412@gmail.com>
2021-09-27 00:21:30 +0300

    lesson3 part2 add classes

--- src/lesson3/Box.java
+++ src/lesson3/Box.java
@@ -5,9 +5,14 @@ import java.util.ArrayList;
 
 public class Box<T extends Fruit> {
     public ArrayList<T> fruits;
+
     public float getWeight() {
         float res = 0;
-        for (T f: fruits) res += f.weight;
+        for (T f : fruits) res += f.weight;
         return res;
+    }
+
+    public boolean compare(Box<T> box) {
+        return this.getWeight() == box.getWeight();
     }
 }
