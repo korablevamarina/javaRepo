907b6efc37e5b6f43b042482ef80af5350997b09
JavaCore <korablevamarina2412@gmail.com>
2021-10-02 22:52:20 +0300

    lesson4 first part

--- src/com/lesson/Main.java
+++ src/com/lesson/Main.java
@@ -1,4 +1,21 @@
 package com.lesson;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        String[] Words = {"a", "b", "b", "c", "d", "d", "e", "f", "j", "j",
+        "i", "f", "f", "g", "h", "h", "k", "l", "m", "n"};
+        Map<String, Integer> map = new HashMap<>();
+        for (String word: Words) {
+            if (map.containsKey(word)) {
+                int cnt = map.get(word);
+                map.put(word, ++cnt);
+            } else {
+                map.put(word, 1);
+            }
+        }
+        System.out.println(map);
+    }
 }
